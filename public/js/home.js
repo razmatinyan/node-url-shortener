6dbcbb29570422b4a66cccde7cae50c5546532d4
razmatinyan <razmatinyan28@gmail.com>
2023-09-08 15:49:45 +0400

    fixed ajax request link

--- public/js/home.js
+++ public/js/home.js
@@ -2,7 +2,7 @@ document.querySelector('#gen-btn').addEventListener('click', shortenURL)
 
 function shortenURL() {
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://127.0.0.1:3000/url', true);
+    xhr.open("POST", 'https://shorturl-u5pu.onrender.com/', true);
     xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
     xhr.send(
         JSON.stringify({
